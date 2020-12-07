72011d906a5169490ade6faab0fa3ac03de52537
Alexi OHearn <ohearnalexi@gmail.com>
2020-12-06 23:47:49 -0500

    minor change

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,9 @@
       <div v-else>
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link> |
-        <router-link to="/map">Map</router-link>
+        <router-link to="/map">Map</router-link> ||
+        <router-link to="/login">Login</router-link> |
+        <router-link to="/register">Register</router-link>
       </div>
     </div>
     <router-view/>
