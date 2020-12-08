284be2367bd5fd115ce810c09fccade0378c75e8
Alexi OHearn <ohearnalexi@gmail.com>
2020-12-07 21:07:33 -0500

    starts restaurants query on MapView component

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -2,11 +2,32 @@
     <div id="map" ref="map"></div>
 </template>
 <script>
+import firebase from 'firebase'
+const db = firebase.firestore()
+
 export default {
     data: () => ({
-        map: null
+        map: null,
+        restaurantsData: []
     }),
+    methods: {
+        getRestaurants() {
+            this.restaurantsData = []
+            db.collection('restaurants')
+                .get()
+                .then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        this.restaurantsData.push({
+                            id: doc.id,
+                            name: doc.data().name,
+                            location: doc.data().location
+                        })
+                    })
+                })
+        }
+    },
     mounted() {
+        console.log("bleh", this.getRestaurants())
         this.map = new window.google.maps.Map(this.$refs["map"], {
             center: { lat: 39.75963977962454, lng: -105.0133786 },
             zoom: 15
